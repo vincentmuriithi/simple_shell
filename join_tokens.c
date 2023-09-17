91ebf85490db9ca3a71d952954dd5006a2c56956
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-17 13:20:32 -0700

    test of alias

--- join_tokens.c
+++ join_tokens.c
@@ -1,38 +1,31 @@
-#include <stdio.h>
 #include "shell.h"
-#include <stdlib.h>
-#include <string.h>
 
 char* join_tokens(char** tokens, int count, const char* delimiter) {
-int i;
 int total_length = 0;
-char* joined_str = NULL;
-
-if (count <= 0 || tokens == NULL) {
-return NULL;
-}
-
+int i;
+int j;
+char* result;
 
 for (i = 0; i < count; i++) {
-total_length += strlen(tokens[i]);
-if (i < count - 1) {
-total_length += strlen(delimiter);
+total_length += strlen(tokens[i]) + strlen(delimiter);
+}
+
+
+result = (char*)malloc(total_length + 1);
+
+if (!result) {
+perror("Memory allocation failed");
+exit(1);
+}
+
+result[0] = '\0';
+
+for (j = 0; j < count; j++) {
+strcat(result, tokens[j]);
+if (j < count - 1) {
+strcat(result, delimiter);
 }
 }
 
-joined_str = (char*)malloc(total_length + 1);
-
-if (joined_str == NULL) {
-perror("Failed to allocate memory for joined string");
-exit(EXIT_FAILURE);
-}
-
-strcpy(joined_str, tokens[0]);
-
-for (i = 1; i < count; i++) {
-strcat(joined_str, delimiter);
-strcat(joined_str, tokens[i]);
-}
-
-return (joined_str);
+return (result);
 }
