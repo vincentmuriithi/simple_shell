965c87c3f1eec5289d5ea18bf117a1b77ef5c736
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-20 02:29:23 -0700

    Reversed code before number 10,- Implement the setenv and unsetenv builtin commands

--- tokenize.c
+++ tokenize.c
@@ -1,24 +1,20 @@
 #include "shell.h"
-
 /**
-* custom_tokenize - Tokenize a string based on a custom delimiter
+* custom_tokenize - Tokenize a string based on spaces
 * @input: The input string
 * @args: An array to store the tokens
-* @delimiter: The delimiter to use for tokenization
 * Return: The number of tokens found
 */
-int custom_tokenize(char *input, char **args, const char *delimiter)
-{
+int custom_tokenize(char *input, char **args) {
 int arg_count = 0;
 char *token;
+char *delimiter = " ";
 
-while (input && *input)
-{
+while (input && *input) {
 while (*input && strchr(delimiter, *input))
 input++;
 
-if (*input)
-{
+if (*input) {
 token = input;
 while (*input && !strchr(delimiter, *input))
 input++;
@@ -32,4 +28,3 @@ args[arg_count++] = token;
 args[arg_count] = NULL;
 return (arg_count);
 }
-
