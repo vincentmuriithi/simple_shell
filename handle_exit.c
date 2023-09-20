c3e9bb257e757b1b00031bd61b845d6fcd469051
vincentmuriithi <vincentmuriithi06@gmail.com>
2023-09-20 14:07:55 -0700

    error resolving solution 3

--- handle_exit.c
+++ handle_exit.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-int handle_exit(char *input, int *status) {
+int handle_exit(char *input, int *status)
+{
 char *status_str = NULL;
 
-if (strcmp(input, "exit") == 0) {
+if (input == NULL || status == NULL)
+return (1);
+
+if (strcmp(input, "exit") == 0)
+{
 exit(0);
-} else if (strncmp(input, "exit ", 5) == 0) {
+}
+else if (strncmp(input, "exit ", 5) == 0)
+{
 status_str = input + 5;
 *status = atoi(status_str);
 return (1);
